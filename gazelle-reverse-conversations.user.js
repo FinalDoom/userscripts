ae34bfba24e985db89f6996623396f3bea8db58c
FinalDoom <7464170-FinalDoom@users.noreply.gitlab.com>
2022-05-26 17:56:58 -0600

    Add menu command to reverse messages if desired

--- gazelle-reverse-conversations.user.js
+++ gazelle-reverse-conversations.user.js
@@ -1,16 +1,20 @@
 // ==UserScript==
 // @name         GazelleGames Conversations Reverse Order
 // @namespace    https://gazellegames.net/
-// @version      0.1.0
+// @version      0.2.0
 // @description  Reverse the order of messages in a PM thread so the newest is on the bottom, next to the reply box.
 // @author       FinalDoom
 // @match        https://gazellegames.net/inbox.php?action=viewconv&id=*
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @grant        none
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
-(function() {
-    'use strict';
+'use strict';
 
+function reverseMessages() {
     $('.linkbox').after($('[id^="message"]:not(#messageform)').parent().toArray().reverse());
-})();
+}
+
+GM_registerMenuCommand('Reverse Conversation Messages', reverseMessages, 'r');
+
+(reverseMessages)();
